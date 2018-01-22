06aa6ce906cad88bf39c3a528fa724bcaa1101dd
Takaaki Hirayama <takaaki.hirayama@bravesoft.co.jp>
2018-01-22 21:05:42 +0900

    使用していない機能を整理する

--- app/src/main/java/jp/co/bravesoft/my_gcm_example2/BackgroundPushNotificationReceiveService.java
+++ app/src/main/java/jp/co/bravesoft/my_gcm_example2/BackgroundPushNotificationReceiveService.java
@@ -1,18 +1,9 @@
 package jp.co.bravesoft.my_gcm_example2;
 
-import android.app.Notification;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import biz.appvisor.push.android.sdk.IAppvisorPushBackgroundService;
 
@@ -22,9 +13,6 @@ import biz.appvisor.push.android.sdk.IAppvisorPushBackgroundService;
 
 public class BackgroundPushNotificationReceiveService extends Service
 {
-    private Context applicationContext  = null;
-    private NotificationManager notificationManager = null;
-    private Intent intent              = null;
 
     @Nullable
     @Override
@@ -35,15 +23,8 @@ public class BackgroundPushNotificationReceiveService extends Service
     @Override
     public int onStartCommand(Intent intent, int flags, int startId)
     {
-        this.applicationContext  = this.getApplicationContext();
-        this.notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-        this.intent              = intent;
-
-        Log.d("onStartCommand", "onStartCommand");
         if (null != intent)
         {
-            Log.d("onStartCommand", notificationMessage());
-
             (new IAppvisorPushBackgroundService() {
                 public void execute() {
                     AsyncNetworkTask task = new AsyncNetworkTask(getApplicationContext());
@@ -55,76 +36,4 @@ public class BackgroundPushNotificationReceiveService extends Service
         return super.onStartCommand(intent, flags, startId);
     }
 
-    private void fireNotification()
-    {
-        NotificationCompat.Builder builder = this.commonNotificationBuilder();
-        builder.setContentIntent(this.contentIntent());
-
-        this.notificationManager.notify(1, this.buildNotification(builder));
-    }
-
-    private NotificationCompat.Builder commonNotificationBuilder()
-    {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
-        builder.setContentTitle("Receive Background Notification");
-        builder.setContentText(this.notificationMessage());
-        builder.setSmallIcon(this.statusbarIconResourceId());
-        builder.setLargeIcon(this.largeIconImage());
-        builder.setStyle(this.notificationStyle());
-
-        return builder;
-    }
-
-    private Intent appIntent()
-    {
-        Intent intent = new Intent(this.applicationContext, MainActivity.class);
-        intent.putExtras(this.intent.getExtras());
-        return intent;
-    }
-
-    private PendingIntent contentIntent()
-    {
-        return PendingIntent.getActivity(this.applicationContext, (int) System.currentTimeMillis(), this.appIntent(), PendingIntent.FLAG_UPDATE_CURRENT);
-    }
-
-    private Notification buildNotification(NotificationCompat.Builder builder)
-    {
-        Notification notification = builder.build();
-        notification.defaults |= Notification.DEFAULT_SOUND;
-        notification.flags = Notification.FLAG_AUTO_CANCEL;
-        return notification;
-    }
-
-    private int statusbarIconResourceId()
-    {
-        return R.mipmap.ic_launcher;
-    }
-
-    private Bitmap largeIconImage()
-    {
-        return BitmapFactory.decodeResource(this.applicationContext.getResources(), R.mipmap.ic_launcher);
-    }
-
-    private NotificationCompat.Style notificationStyle()
-    {
-        return new NotificationCompat.BigTextStyle().bigText(this.notificationMessage());
-    }
-
-    private String notificationMessage()
-    {
-        Bundle bundle = this.intent.getExtras();
-
-        StringBuilder stringBuilder = new StringBuilder();
-        for (String key : bundle.keySet())
-        {
-            Object value = bundle.get(key);
-            if (!(value instanceof String))
-            {
-                value = value.toString();
-            }
-            stringBuilder.append(String.format("%s: %s%s", key, value, System.getProperty("line.separator")));
-        }
-
-        return stringBuilder.toString();
-    }
 }
--- app/src/main/java/jp/co/bravesoft/my_gcm_example2/MyJobService.java
+++ app/src/main/java/jp/co/bravesoft/my_gcm_example2/MyJobService.java
@@ -2,52 +2,25 @@ package jp.co.bravesoft.my_gcm_example2;
 
 import android.app.job.JobParameters;
 import android.app.job.JobService;
-import android.os.AsyncTask;
-import android.util.Log;
-import android.widget.Toast;
 
 import biz.appvisor.push.android.sdk.IAppvisorPushBackgroundService;
 
 public class MyJobService extends JobService {
-    private static final String TAG = "MyJobService";
 
     @Override
     public boolean onStartJob(JobParameters params) {
-        new ToastTask().execute(params);
+        (new IAppvisorPushBackgroundService() {
+            public void execute() {
+                AsyncNetworkTask task = new AsyncNetworkTask(getApplicationContext());
+                task.execute("http://dev-p.app-visor.com/");
+            }
+        }).execute();
         return true;
     }
 
     @Override
     public boolean onStopJob(JobParameters params) {
         return false;
-    }
-
-
-    private class ToastTask extends AsyncTask<JobParameters, Void, String> {
-
-        protected JobParameters mJobParam;
-
-        @Override
-        protected String doInBackground(JobParameters... params) {
-            Log.d(TAG, "doInBackground..");
-            mJobParam = params[0];
-
-            (new IAppvisorPushBackgroundService() {
-                public void execute() {
-                    AsyncNetworkTask task = new AsyncNetworkTask(getApplicationContext());
-                    task.execute("http://dev-p.app-visor.com/");
-                }
-            }).execute();
-
-            return String.valueOf(mJobParam.getJobId());
-        }
-
-        @Override
-        protected void onPostExecute(String result) {
-            Log.d(TAG, "onPostExecute!");
-            //jobFinished(mJobParam, false);
-            //Toast.makeText(MyJobService.this, "job id = " + result, Toast.LENGTH_SHORT).show();
-        }
     }
 
 }
